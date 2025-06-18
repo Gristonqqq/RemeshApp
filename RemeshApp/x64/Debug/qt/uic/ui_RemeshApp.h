3351d2d368b8bccbd7f9a197686fcaecf78ca202
Gr1stonqqq <sashatroan@gmail.com>
2025-06-18 12:34:52 +0300

    Created form design for main screen

--- RemeshApp/x64/Debug/qt/uic/ui_RemeshApp.h
+++ RemeshApp/x64/Debug/qt/uic/ui_RemeshApp.h
@@ -11,10 +11,9 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QFrame>
+#include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QMainWindow>
-#include <QtWidgets/QMenuBar>
-#include <QtWidgets/QStatusBar>
-#include <QtWidgets/QToolBar>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -22,28 +21,48 @@ QT_BEGIN_NAMESPACE
 class Ui_RemeshAppClass
 {
 public:
-    QMenuBar *menuBar;
-    QToolBar *mainToolBar;
     QWidget *centralWidget;
-    QStatusBar *statusBar;
+    QHBoxLayout *horizontalLayout_2;
+    QFrame *ViewPort;
+    QFrame *MenuTab;
 
     void setupUi(QMainWindow *RemeshAppClass)
     {
         if (RemeshAppClass->objectName().isEmpty())
             RemeshAppClass->setObjectName("RemeshAppClass");
-        RemeshAppClass->resize(600, 400);
-        menuBar = new QMenuBar(RemeshAppClass);
-        menuBar->setObjectName("menuBar");
-        RemeshAppClass->setMenuBar(menuBar);
-        mainToolBar = new QToolBar(RemeshAppClass);
-        mainToolBar->setObjectName("mainToolBar");
-        RemeshAppClass->addToolBar(mainToolBar);
+        RemeshAppClass->resize(846, 531);
         centralWidget = new QWidget(RemeshAppClass);
         centralWidget->setObjectName("centralWidget");
+        horizontalLayout_2 = new QHBoxLayout(centralWidget);
+        horizontalLayout_2->setSpacing(6);
+        horizontalLayout_2->setContentsMargins(11, 11, 11, 11);
+        horizontalLayout_2->setObjectName("horizontalLayout_2");
+        horizontalLayout_2->setContentsMargins(1, 1, 1, 1);
+        ViewPort = new QFrame(centralWidget);
+        ViewPort->setObjectName("ViewPort");
+        ViewPort->setEnabled(true);
+        QSizePolicy sizePolicy(QSizePolicy::Policy::Preferred, QSizePolicy::Policy::Preferred);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(ViewPort->sizePolicy().hasHeightForWidth());
+        ViewPort->setSizePolicy(sizePolicy);
+        ViewPort->setStyleSheet(QString::fromUtf8("background-color: rgb(211, 211, 211);"));
+        ViewPort->setFrameShape(QFrame::Shape::StyledPanel);
+        ViewPort->setFrameShadow(QFrame::Shadow::Raised);
+
+        horizontalLayout_2->addWidget(ViewPort);
+
+        MenuTab = new QFrame(centralWidget);
+        MenuTab->setObjectName("MenuTab");
+        MenuTab->setStyleSheet(QString::fromUtf8("background-color: rgb(131, 131, 131);"));
+        MenuTab->setFrameShape(QFrame::Shape::StyledPanel);
+        MenuTab->setFrameShadow(QFrame::Shadow::Raised);
+
+        horizontalLayout_2->addWidget(MenuTab);
+
+        horizontalLayout_2->setStretch(0, 8);
+        horizontalLayout_2->setStretch(1, 2);
         RemeshAppClass->setCentralWidget(centralWidget);
-        statusBar = new QStatusBar(RemeshAppClass);
-        statusBar->setObjectName("statusBar");
-        RemeshAppClass->setStatusBar(statusBar);
 
         retranslateUi(RemeshAppClass);
 
